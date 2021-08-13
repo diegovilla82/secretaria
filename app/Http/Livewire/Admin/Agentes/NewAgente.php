503d5995c35ee2996fe930191bc1aabffa76d67d
Federico Barraza <fede.barraza@gmail.com>
2021-08-13 12:53:56 -0300

    carga de agentes funcionando

--- app/Http/Livewire/Admin/Agentes/NewAgente.php
+++ app/Http/Livewire/Admin/Agentes/NewAgente.php
@@ -3,19 +3,48 @@
 namespace App\Http\Livewire\Admin\Agentes;
 
 use App\Cargo;
+use App\Agente;
 use Livewire\Component;
 
 class NewAgente extends Component
 {
-    public $disabled, $situacion_revista_selected, $escalafon_selected;
+    public $disabled, $situacion_revista_selected = "Contrato de Servicio", $escalafon_selected = "General del Poder Ejecutivo", 
+    $categoria_selected = "Personal Adm. y Tecnico" , $cargo_selected = 1, $agente;
 
-    public function mount()
-    {
+    protected $rules = [
+        'agente.nombre'                 => '',
+        'agente.cuit'                   => '',
+        'situacion_revista_selected'    => '',
+        'escalafon_selected'            => '',
+        'categoria_selected'            => '',
+        'cargo_selected'                => '',
+        'agente.apartado'               => '',
+        'agente.ceic'                   => '',
+        'agente.grupo'                  => '',
+        'agente.numero'                 => ''
+    ];
 
+    public function mount() {
+        $this->agente = new Agente();
     }
+    
+    public function save_agente() {
+        $this->agente->situacion_revista_id = $this->situacion_revista_selected;
+        $this->agente->escalafon_id = $this->escalafon_selected;
+        $this->agente->categoria_id = $this->categoria_selected;
+        $this->agente->cargo_id = $this->cargo_selected;
+        $this->agente->save();
+
+        return redirect()->route('comisiones_lw.index');
+    }
+
     public function render()
     {
-        $array_situacion_revista = collect( ["Contrato de Servicio", "Funcionario", "Planta Permanente", "Personal Transitorio", "Personal de Gabinete", "Personal adscripto" ]);
+        $array_situacion_revista = collect( [ "Contrato de Servicio" => "Contrato de Servicio", 
+            "Funcionario" => "Funcionario", "Planta Permanente" => "Planta Permanente", 
+            "Personal Transitorio" => "Personal Transitorio", "Personal de Gabinete" => "Personal de Gabinete", 
+            "Personal adscripto" => "Personal adscripto" 
+        ]);
         $array_escalafon = collect( ["General del Poder Ejecutivo", "Aut. Sup. del Poder Ejecutivo", "Otro"]);
         $array_categoria = collect( ["Personal Adm. y Tecnico", "Personal de Servicios", "Personal de Gabinete", "Personal administrativo y tecnico" ]); 
         $array_cargo = Cargo::orderBy('nombre')->pluck('nombre','id');
--- resources/views/livewire/admin/agentes/form.blade.php
+++ resources/views/livewire/admin/agentes/form.blade.php
@@ -1,17 +1,40 @@
 <div class="row">
     <x-admin.input :disabled="$disabled" title="Nombre y apellido" model="agente.nombre" required=true tabindex=1 classes="col-md-12" />
-    <x-admin.input :disabled="$disabled" title="CUIT" model="agente.cuit" required=true tabindex=1 classes="col-md-12" />
+    <x-admin.input :disabled="$disabled" title="CUIT" model="agente.cuit" required=true tabindex=2 classes="col-md-12" />
     <x-admin.select 
+        required=true
         model="situacion_revista_selected" 
         title="Situacion de revista:" 
         :values="$situaciones_revista" 
         classes="col-md-12" 
-        tabindex=1
+        tabindex=3
     />
-    <x-admin.input :disabled="$disabled" title="Situacion de revista" model="agente.situacion_revista_id" required=true tabindex=1 classes="col-md-12" />
-    <x-admin.input :disabled="$disabled" title="Escalafon" model="agente.escalafon_id" required=true tabindex=1 classes="col-md-12" />
-    <x-admin.input :disabled="$disabled" title="Categoria" model="agente.categoria_id" required=true tabindex=1 classes="col-md-12" />
-    <x-admin.input :disabled="$disabled" title="Apartado" model="agente.cargo_id" required=true tabindex=1 classes="col-md-12" />
-    <x-admin.input :disabled="$disabled" title="Ceic" model="agente.ceic" required=true tabindex=1 classes="col-md-12" />
-    <x-admin.input :disabled="$disabled" title="Grupo" model="agente.numero" required=true tabindex=1 classes="col-md-12" />
+    <x-admin.select 
+        required=true
+        model="escalafon_selected" 
+        title="Escalafon:" 
+        :values="$escalafones" 
+        classes="col-md-12" 
+        tabindex=4
+    />
+    <x-admin.select
+        model="categoria_selected" 
+        title="Categoria:" 
+        :values="$categorias" 
+        classes="col-md-12" 
+        tabindex=5
+    />
+
+    <x-admin.input :disabled="$disabled" title="Apartado" model="agente.apartado" required=true tabindex=6 classes="col-md-12" />
+    <x-admin.select
+        required=true
+        model="cargo_selected" 
+        title="Cargo:" 
+        :values="$cargos" 
+        classes="col-md-12" 
+        tabindex=7
+    />
+    <x-admin.input :disabled="$disabled" title="Ceic" model="agente.ceic" required=true tabindex=8 classes="col-md-12" />
+    <x-admin.input :disabled="$disabled" title="Grupo" model="agente.grupo" required=true tabindex=9 classes="col-md-12" />
+    <x-admin.input :disabled="$disabled" title="Numero" model="agente.numero" required=true tabindex=10 classes="col-md-12" />
 </div>
